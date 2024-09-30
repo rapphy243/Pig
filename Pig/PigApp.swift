24133d53c445aeb9f598e5a8c60addb0feeaf1c3
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-09-30 09:45:58 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Raphael Abano on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
