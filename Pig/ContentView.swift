eaaf4132ffd8a88243f92dc434798248e396ebb7
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-09-30 10:06:04 -0500

    Upload Progress Step 4

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var turnScore = 0
+    @State var gameScore = 0
     var body: some View {
-        VStack {
-            Text("Pig")
+        ZStack {
+            Color.gray.opacity(0.7).ignoresSafeArea()
+            VStack {
+                Image("Pig")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                Text("Pig")
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
