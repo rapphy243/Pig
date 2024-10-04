1917769c7979566312af3e1a3fe96ed0b7fd2ad3
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-10-03 21:02:22 -0500

    Upload Progress (Step 15)

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State var turnScore = 0
     @State var gameScore = 0
+    @State var randomValue = 0
+    @State var rotation = 0.0
     var body: some View {
         ZStack {
             Color.gray.opacity(0.7).ignoresSafeArea()
@@ -17,10 +19,77 @@ struct ContentView: View {
                 Image("Pig")
                     .resizable()
                     .frame(width: 150, height: 150)
-                Text("Pig")
+                CustomText(text: "Pig")
+                Image("pips \(randomValue)")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .rotationEffect(.degrees(rotation))
+                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                    .padding(50)
+                CustomText(text: "Turn Score: \(turnScore)")
+                HStack {
+                    Button("roll"){
+                        chooseRandom(times: 3)
+                        withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                            rotation += 360
+                        }
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                    Button("Hold"){
+                        gameScore += turnScore
+                        endTurn()
+                        withAnimation(.easeInOut(duration: 1)){
+                            rotation += 360
+                        }
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                }
+                CustomText(text: "Game Score: \(gameScore)")
                 Spacer()
             }
         }
+    }
+    
+    func endTurn() {
+        turnScore = 0
+        randomValue = 0
+    }
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
+        }
+        if times == 0 {
+            if randomValue == 1 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    endTurn()
+                }
+            }
+            else {
+                turnScore += randomValue
+            }
+        }
+    }
+}
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text)
+            .font(Font.custom("Marker Felt", size: 36))
+    }
+}
+
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 50)
+            .font(Font.custom("Marker Felt", size: 24))
+            .padding()
+            .background(Color.red).opacity(configuration.isPressed ? 0 : 1.0)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
